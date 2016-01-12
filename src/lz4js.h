fe2cf90335dd85a853145f4a2736852b7097010b
ukyo <ukyo.web@gmail.com>
2016-01-12 19:48:55 +0900

    update header

--- src/lz4js.h
+++ src/lz4js.h
@@ -1,6 +1,29 @@
 #include <cstddef>
 #include <lz4frame.h>
 #include <emscripten.h>
+
+
+typedef struct {
+  unsigned int id,
+  size_t dstMaxSize,
+  LZ4F_compressionContext_t ctx,
+  LZ4F_preferences_t preferences
+} LZ4JS_compressionContext_t;
+
+typedef struct {
+  unsigned int id;
+  LZ4F_decompressionContext_t ctx
+} LZ4JS_decompressionContext_t;
+
+LZ4JS_compressionContext_t* LZ4JS_createCompressionContext(unsigned int id, unsigned int compressionLevel);
+void LZ4JS_freeCompressionContext(LZ4JS_compressionContext_t* ctx);
+void LZ4JS_compressBegin(LZ4JS_compressionContext_t* ctx);
+void LZ4JS_compressUpdate(LZ4JS_compressionContext_t* ctx);
+void LZ4JS_compressEnd(LZ4JS_compressionContext_t* ctx);
+LZ4JS_decompressionContext_t* LZ4JS_createDecompressContext(unsigned int id);
+void LZ4JS_freeDecompressContext(LZ4JS_decompressionContext_t* ctx);
+void LZ4JS_decompress(LZ4JS_decompressionContext_t* ctx)
+
 #include <emscripten/bind.h>
 
 using namespace std;
